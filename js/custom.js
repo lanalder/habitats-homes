7bec6f346ad906d26315668055d2bf61489d0c96
lanalder <lalanalder@gmail.com>
2021-04-07 12:04:44 +1200

    ack

--- js/custom.js
+++ js/custom.js
@@ -7,21 +7,21 @@ const length = rooms.length;
 let counter = 0;
 
 prevBtn.onclick = function(){
-  rooms[counter].style.display = 'none';
+  rooms[counter].classList.toggle('hiding');
   if (counter === 0){
     counter = length - 1;
   } else {
     counter --;
   }
-  rooms[counter].style.display = 'block';
+  rooms[counter].classList.toggle('hiding');
 }
 
 nextBtn.onclick = function(){
-  rooms[counter].style.display = 'none';
+  rooms[counter].classList.toggle('hiding');
   if (counter === length - 1){
     counter = 0;
   } else {
     counter ++;
   }
-  rooms[counter].style.display = 'block';
+  rooms[counter].classList.toggle('hiding');
 }
