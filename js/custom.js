96357753f5863312f46a54d1d5096b4330f2516d
lanalder <lalanalder@gmail.com>
2021-04-11 23:51:46 +1200

    and to test us god granted us whatever the hell closure is about

--- js/custom.js
+++ js/custom.js
@@ -9,30 +9,146 @@ const footerText = document.querySelectorAll('.footer-text');
 
 
 let counter = 0;
-let i;
+// let i;
+var noClick = 0;
+var a = Array.from(redBtns);
+var f = Array.from(footerText);
+// let t;
 
-function removeText(){
-  for (i=0; i < footerText.length; i++){
-    footerText[i].classList.add('hiding');
-    addText();
-  }
+//
+// for (let i=0; i < redBtns.length; i++){
+//   (function(i){
+//     redBtns[i].onclick = function(){
+//       return function(){
+//         const t = a.indexOf(redBtns[i]);
+//         console.log(t);
+//       };
+//     };
+//   })(i);
+// }
+
+// for (var i=0; i < redBtns.length; i++){
+//   redBtns[i].onclick = function(){
+//     t = a.indexOf(redBtns[i]);
+//   };
+// }
+//
+// for (var b= 0; b < redBtns.length; b++){
+//   redBtns[b]();
+// }
+
+let t;
+var tch;
+
+// for (var i=0; i < redBtns.length; i++){
+//   redBtns[i].onclick = function(e){
+//     return function(e){
+//       t = a.indexOf(e.currentTarget);
+//     };
+//   }(t);
+// }
+
+console.log(tch);
+
+
+for (var i=0; i < redBtns.length; i++){
+  redBtns[i].addEventListener('click', function(e){
+    var tch = function(e){
+      return function(e){
+        t = a.indexOf(e.currentTarget);
+      };
+    }(t);
+  })
 }
 
-function addText(){
-  for (i=0; i < redBtns.length; i++){
-    redBtns[i].onclick = function(e){
-      let a = Array.from(redBtns);
-      let t = a.indexOf(e.target);
-      let f = Array.from(footerText);
-      f[t].classList.toggle('hiding');
-      for (i=0; i < redBtns.length; i++){
-        redBtns[i].onclick = removeText();
-      }
-    }
-  }
-}
+// function setBtn(i){
+//   return function(){
+//     t = a.indexOf(i.currentTarget);
+//     console.log(t);
+//     // setElement(i);
+//     // setIndex(i);
+//     // return false;
+//   };
+// }
 
-addText();
+// function setElement(i){
+//   t = a.indexOf(i.currentTarget);
+//   console.log(t);
+// }
+//
+// function setIndex(i){
+//   t = a.indexOf(i.currentTarget);
+//   console.log(t);
+// }
+
+// console.log(t);
+
+// whiteText();
+//
+// function whiteText(){
+//   if (noClick === 0){
+//     for (i=0; i < redBtns.length; i++){
+//       redBtns[i].onclick = function(e){
+//         console.dir(e);
+//         t = a.indexOf(e.currentTarget);
+//         f[t].classList.remove('hiding');
+//         noClick++;
+//       }
+//     }
+//   } else {
+//     for (i=0; i < f.length; i++){
+//       console.log(noClick);
+//       f[i].classList.add('hiding');
+//       noClick = 0;
+//     }
+//   }
+// }
+// function whiteText(){
+//   for (i=0; i < redBtns.length; i++){
+//     redBtns[i].onclick = function(e){
+//       console.log(redBtns[i]);
+//       if (noClick === 0){
+//         function unamed(e){
+//           console.log(e.currentTarget);
+//           t = a.indexOf(e.currentTarget);
+//           f[t].classList.remove('hiding');
+//           noClick++;
+//         }
+//       } else {
+//         for (i=0; i < f.length; i++){
+//           // console.log(noClick);
+//           f[i].classList.add('hiding');
+//           noClick = 0;
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+
+// function toggleText(t){
+//   if (noClick > 1){
+//     console.log(f[t]);
+//     f[t].classList.toggle('hiding');
+//     noClick = 0;
+//     t = undefined;
+//   } else {
+//     return;
+//   }
+// }
+
+// for (i=0; i < redBtns.length; i++){
+//   redBtns[i].onclick = function(e, noClick++){
+//     let a = Array.from(redBtns);
+//     let t = a.indexOf(e.target);
+//     let f = Array.from(footerText);
+//     f[t].classList.toggle('hiding');
+//   }
+// }
+
+
+
 
 prevBtn.onclick = function(){
   rooms[counter].classList.toggle('hiding');
